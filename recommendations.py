640d9d18b4dfffd1b86b5f7f01747d7898a9d96c
momchilldd <66889211+momchilldd@users.noreply.github.com>
2024-05-20 11:48:25 +0300

    Delete comments

--- recommendations.py
+++ recommendations.py
@@ -4,13 +4,9 @@ import random
 from model_utils import load_model_and_tokenizer, get_meal_embeddings
 from db import meals, prices
 
-# Load the model and tokenizer
 tokenizer, model = load_model_and_tokenizer()
-
-# Convert meal names to embeddings
 meal_embeddings = get_meal_embeddings(meals, tokenizer, model)
 
-# Build Faiss index
 embedding_dim = model.config.hidden_size
 index = faiss.IndexFlatL2(embedding_dim)
 index.add(meal_embeddings.detach().numpy())
@@ -22,21 +18,16 @@ def recommend_meal(preference, budget):
     last_hidden_states = outputs.last_hidden_state
     pooled_output = last_hidden_states.mean(dim=1)
 
-    # Search for the closest meal in the vector database
     preference_embedding = torch.zeros(1, embedding_dim)
     preference_embedding[0] = pooled_output
     _, similar_meals = index.search(preference_embedding.detach().numpy(), 1)
 
-    # Check if there are any actual meals depending on our search
     preference_words = set(preference.lower().split())
     meal_words = set(word.lower() for meal in meals for word in meal.split())
     if not bool(preference_words & meal_words):
         return "Sorry, couldn't find meals based on your search."
 
-    # Set meals within budget
     meals_within_budget = [meals[i] for i in similar_meals[0] if prices[i] <= budget]
-
-    # If no meals are found within the budget:
     if not meals_within_budget:
         return "Sorry, couldn't find meals within your budget."
 
